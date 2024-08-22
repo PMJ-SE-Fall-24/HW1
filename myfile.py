49836a0e0c6541199060517d2eff631f7fb1c379
Parth291102 <157012735+Parth291102@users.noreply.github.com>
2024-08-22 20:33:39 +0000

    Error Introduced

--- myfile.py
+++ myfile.py
@@ -1,11 +1,11 @@
 def fibonacci(n):
     a, b = 0, 1
     sequence = []
-    for _ in range(n):
+    for x in range(n):
         sequence.append(a)
         a, b = b, a + b
     return sequence
 
-terms = int(input("Enter the number of terms: "))
+terms = input("Enter the number of terms: ")
 print(f"Fibonacci sequence up to {terms} terms:")
 print(fibonacci(terms))
