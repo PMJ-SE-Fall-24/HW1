479e5f8f0df7d9ec017217a17ec3e5447f2bd615
Parth291102 <157012735+Parth291102@users.noreply.github.com>
2024-09-11 23:25:31 +0000

    Test Cases Done

--- test_myfile.py
+++ test_myfile.py
@@ -4,4 +4,4 @@ def test_1():
     assert myfile.fibonacci(5) == [0, 1, 1, 2, 3]
 
 def test_2():
-    assert myfile.fibonacci(6) == [0, 1, 1, 2, 3, 3]
+    assert myfile.fibonacci(6) == [0, 1, 1, 2, 3, 5]
