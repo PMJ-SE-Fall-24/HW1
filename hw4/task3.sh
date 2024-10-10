78596735bde54f5e9b5b2b084aec53e2d4ccfd1e
Parth291102 <157012735+Parth291102@users.noreply.github.com>
2024-10-10 02:55:28 +0000

    HW4_Task3

--- hw4/task3.sh
+++ hw4/task3.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 
-gawk -F',' 'NR > 1 && $3 == 2 && substr($13, 1, 1) == "S" { gsub(/,male/, ",M"); gsub(/,female/, ",F"); 
-total += $7; count++ } END { print (count ? total / count : 0) }' titanic.csv 
+sed 's/^,/NULL,/; :a;s/,,/,NULL,/g;ta' titanic.csv > updated_titanic.csv 
+
+gawk -F',' '$13 ~ /S\s*$/ && $3 == 2 { print }' updated_titanic.csv | sed 's/male/M/g; s/female/F/g' | awk -F',' '$7 != "NULL" { sum += $7; count++ } END { if (count > 0) print sum / count;}'
